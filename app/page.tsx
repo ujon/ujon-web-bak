78b1239258c4dc1b3a7ffbb2d36d3120df6c31cc
ujon <ujon94@gmail.com>
2024-08-12 01:01:31 -0700

    feat: modify home page height
    # Manual description
    
    # Auto description
    Change the hero section's height from "calc(100vh - 4rem)" to a fixed "90vh" to ensure consistent display across different screen resolutions. This adjustment simplifies the layout and helps maintain design uniformity.

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ function Home() {
     const text1 = "Hello World";
     const text2 = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
     return (
-        <div className="hero flex flex-col justify-center items-start" style={{height: "calc(100vh - 4rem)"}}>
+        <div className="hero flex flex-col justify-center items-start" style={{height: "90vh"}}>
             <div className="hero-content text-left">
                 <div className="max-w-screen-md">
                     <h1 className="text-5xl font-bold">{text1}</h1>
