f0ea3a908fbbe620cd0e94f9728058b5d8ead77d
ujon <ujon94@gmail.com>
2024-08-20 15:10:08 -0700

    chore: relocate messages folder
    # Manual description
    
    # Auto description

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,9 +3,9 @@ import {getUserLocale} from "@/services/locale";
 
 export default getRequestConfig(async () => {
     const locale = await getUserLocale();
-    
+
     return {
         locale,
-        messages: (await import(`../messages/${locale}.json`)).default
+        messages: (await import(`./messages/${locale}.json`)).default
     };
 });
