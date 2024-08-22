f4a2693f33439dba954198d841077f22c611f6e6
ujon <ujon94@gmail.com>
2024-08-21 18:28:26 -0700

    asset: aad email, github, linkedin icons
    # Manual description
    
    # Auto description

---
+++ src/constants/icons/Email.tsx
@@ -0,0 +1,19 @@
+import type {Icon} from "ujon-constants";
+
+function Email({size = '24', color = 'black'}: Icon) {
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={size}
+            height={size}
+            viewBox="0 0 24 24"
+            fill={color}
+        >
+            <path d="M12 13.183L19 8.75934V17H5V8.75934L12 13.183Z"/>
+            <path d="M5.95982 7H18.0402L12 10.817L5.95982 7Z"/>
+            <path fillRule="evenodd" clipRule="evenodd" d="M24 0H0V24H24V0ZM21 6.3125V5H3V6.3125V19H21V6.3125Z"/>
+        </svg>
+    )
+}
+
+export default Email;
