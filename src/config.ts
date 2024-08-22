3cba30c19cf6ab35d03ff446e16167971eeaa722
ujon <ujon94@gmail.com>
2024-08-21 18:29:39 -0700

    feat: relocate messages folder & config contact information
    # Manual description
    
    # Auto description

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,21 @@
+// Locales
 export type Locale = (typeof locales)[number];
-
 export const locales: string[] = ['en', 'ko'];
 export const localeLabels: { [key in Locale]: string } = {
     en: 'English',
     ko: '한국어',
 };
 export const defaultLocale: Locale = 'en';
+
+// Contacts
+export interface Contacts {
+    linkedin: string;
+    github: string;
+    email: string;
+}
+
+export const contacts: Contacts = {
+    "linkedin": "https://www.linkedin.com/in/ujon",
+    "github": "https://github.com/ujon",
+    "email": "ujon@gmail.com",
+}
